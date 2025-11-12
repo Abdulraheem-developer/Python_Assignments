b1a117f3041fb35ed42b78b341f76e53cb4a1e68
Abdulraheem Sanni <sannimayowa24@gmail.com>
2025-11-12 23:42:55 +0100

    Circle Task With python

---
+++ Circle.py
@@ -0,0 +1,11 @@
+radius = 2 
+PI = 3.14159
+
+diameter = 2 * radius 
+print(f"The diameter of the circle is {diameter}")
+
+circumference = 2 * PI * radius
+print(f"The circumference of the circle is {circumference}");
+
+area = PI * radius * radius
+print(f"The area of the circle is {area}")
