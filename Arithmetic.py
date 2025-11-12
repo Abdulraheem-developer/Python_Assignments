1515d46779188f0852753cec09bc44777b3ec72b
Abdulraheem Sanni <sannimayowa24@gmail.com>
2025-11-12 23:44:03 +0100

    Arithmetic Task

---
+++ Arithmetic.py
@@ -0,0 +1,17 @@
+addition = 27.5 + 2
+print(addition)
+
+subtraction = 27.5 - 2
+print(subtraction)
+
+multiplication = 27.5 * 2
+print(multiplication)
+
+division = 27.5 / 2 
+print(division)
+
+floor = 27.5 // 2
+print(floor)
+
+power = 27.5 ** 2
+print(power)
